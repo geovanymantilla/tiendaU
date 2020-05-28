254762cff669f96a7cb5a5d0d9244f7b01bd3f23
geovanymantilla <geovanymantilla@gmail.com>
2020-05-28 07:20:11 -0500

    first commit

---
+++ src/entities/Servicio.java
@@ -0,0 +1,64 @@
+package entities;
+
+import java.io.Serializable;
+import javax.persistence.*;
+
+
+/**
+ * The persistent class for the servicio database table.
+ * 
+ */
+@Entity
+@NamedQuery(name="Servicio.findAll", query="SELECT s FROM Servicio s")
+public class Servicio implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private int id;
+
+	private Object descripcion;
+
+	private String nombre;
+
+	//bi-directional many-to-one association to Tienda
+	@ManyToOne
+	@JoinColumn(name="tienda")
+	private Tienda tiendaBean;
+
+	public Servicio() {
+	}
+
+	public int getId() {
+		return this.id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public Object getDescripcion() {
+		return this.descripcion;
+	}
+
+	public void setDescripcion(Object descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public String getNombre() {
+		return this.nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public Tienda getTiendaBean() {
+		return this.tiendaBean;
+	}
+
+	public void setTiendaBean(Tienda tiendaBean) {
+		this.tiendaBean = tiendaBean;
+	}
+
+}
